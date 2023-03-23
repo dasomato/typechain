e8c1979deaca1b9a0130219bd3819df441db72f7
merkuree <merkuree@naver.com>
2023-03-23 15:43:57 +0900

    polymorphism

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
-class Block {
-  constructor(
-    private data:string,
-  ) {}
-  hello(): string {
-    return this.data;
-  }
+type SuperPrintExample = {
+  (arr: number[]): void,
+  (arr: string[]): void,
+  (arr: boolean[]): void,
+  (arr: (number|boolean)[]): void,
+};
+type SuperPrint = {
+  <T>(arr: T[]): T,
+  <T>(arr: T[], no:number): T,
+};
+
+const superPrint: SuperPrint = (arr, no?) => {
+  if(no) return arr[no];
+  return arr[0];
 }
 
-const block = new Block("abc");
-console.log(block.hello());
+const superPrint2: SuperPrint = (arr) => {
+  return arr[0];
+}
+
+
+const a = superPrint([1, 2, 3, 4])
+const b = superPrint([true, false, true], 1)
+const c = superPrint(["a", "b", "c"], 2)
+const d = superPrint([1, 2, true, false])
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
